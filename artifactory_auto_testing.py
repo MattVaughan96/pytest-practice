43af2c5a82c4ffcc01dd50643fd8363296ab8bd6
Matt Vaughan <86467372+MattVaughan96@users.noreply.github.com>
2021-07-21 16:00:38 +0100

    Create artifactory_auto_testing.py

---
+++ artifactory_auto_testing.py
@@ -0,0 +1,8 @@
+import pytest
+import requests
+import artifactory
+def check_status_code_equals_200():
+    url = "https://binarycentral.jfrog.io/artifactory/docker-hub/library"
+    auth = HTTPBasicAuth(username,apiKey)
+    response = requests.get(url,headers=headers,auth=auth,verify = "",proxies = proxyDict)
+    assert response.status_code == 200
