b072e5fd39f548d55d2990695de48e51f1d5895d
Matt Vaughan <mvaughan96@gmail.com>
2021-07-21 17:08:48 +0100

    new folder made

--- automated testing/artifactory_auto_testing.py
+++ automated testing/artifactory_auto_testing.py
@@ -7,3 +7,12 @@ def check_status_code_equals_200():
     auth = HTTPBasicAuth(username,apiKey)
     response = requests.get(url,headers=headers,auth=auth,verify = "",proxies = proxyDict)
     assert response.status_code == 200
+
+def Check_package_download():
+    url = "https://binarycentral.jfrog.io/artifactory/docker-hub/library"
+    auth = HTTPBasicAuth(username,apiKey)
+    response = requests.get(url,headers=headers,auth=auth,verify = "",proxies = proxyDict)
+
+    #code that pip installs docker package
+    
+    #code to check installed successfully
