7d853417a39b20de6dfbec4df7766aa39d9f292f
Matt Vaughan <86467372+MattVaughan96@users.noreply.github.com>
2021-07-22 14:46:17 +0100

    Update artifactory_auto_testing.py

--- automated testing/artifactory_auto_testing.py
+++ automated testing/artifactory_auto_testing.py
@@ -2,6 +2,10 @@ import pytest
 import requests
 import artifactory
 
+def test_get_locations_for_us_90210_check_status_code_equals_200():
+     response = requests.get("http://api.zippopotam.us/us/90210")
+     assert response.status_code == 200
+
 def check_status_code_equals_200():
     url = "https://binarycentral.jfrog.io/artifactory/docker-hub/library"
     auth = HTTPBasicAuth(username,apiKey)
