7d3de8281e384e5754f05954eb8b9341d9f8658f
Matt Vaughan <86467372+MattVaughan96@users.noreply.github.com>
2021-07-27 17:00:11 +0100

    Update artifactory_auto_testing.py

--- automated_testing/artifactory_auto_testing.py
+++ automated_testing/artifactory_auto_testing.py
@@ -1,23 +1,36 @@
 import pytest
 import requests
 from artifactory import ArtifactoryPath
+from dohq_artifactory import generate_password, User
 
 USERNAME = "name"
 PASSWORD = "password"
 
 
+#find user
+user = artifactory_.find_user("username")
+#creates new user
+if user is None:
+    # User does not exist
+    user = User(
+        artifactory_, "username", "username@bankofengland.co.uk", password=generate_password()
+    )
+    user.create()
+
 path = ArtifactoryPath(
     "https://binarycentral.jfrog.io/artifactory/docker-hub/library",
-    auth=(USERNAME, PASSWORD),
+    auth=(USERNAME, PASSWORD) #user.username, user.password
 )
 #downloads artifact
 with path.open() as fd, open("tomcat.tar.gz", "wb") as out:
     out.write(fd.read())
 
+#test page has a reponse
 def test_status_code_equals_200():
     response = requests.get(path)
     assert response.status_code == 200
 
+#test the artifact is downloaded
 def test_package_download():
     response = requests.get(path)
     assert 
