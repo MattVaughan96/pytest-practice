42484a02a82ee18461bc8e2956be845c4604ddcb
Matt Vaughan <86467372+MattVaughan96@users.noreply.github.com>
2021-07-28 14:37:50 +0100

    Update artifactory_auto_testing.py

--- automated_testing/artifactory_auto_testing.py
+++ automated_testing/artifactory_auto_testing.py
@@ -1,25 +1,29 @@
 import pytest
 import requests
 from artifactory import ArtifactoryPath
-from dohq_artifactory import generate_password, User
 
-USERNAME = "name"
+
+USERNAME = "Service_Account"
 PASSWORD = "password"
-
+#-cyberark generate password 
 
 path = ArtifactoryPath(
     "https://binarycentral.jfrog.io/artifactory/docker-hub/library",
-    auth=(USERNAME, PASSWORD) #user.username, user.password
+    auth=(USERNAME, PASSWORD)
 )
-#downloads artifact
-with path.open() as fd, open("tomcat.tar.gz", "wb") as out:
-    out.write(fd.read())
+
+
 
 #test page has a reponse
 def test_status_code_equals_200():
     response = requests.get(path)
     assert response.status_code == 200
 
+
+#downloads artifact
+with path.open() as fd, open("tomcat.tar.gz", "wb") as out:
+    out.write(fd.read())
+    
 #test the artifact is downloaded
 def test_package_download():
     response = requests.get(path)
@@ -29,3 +33,11 @@ def test_package_download():
   https://stackoverflow.com/questions/1051254/check-if-python-package-is-installed
     
     #code to check installed successfully
+
+    
+"""1. POC accessing repo - using dummy account - 2
+2. security - access password though cyberark - create safe - dynamic generate password - 5
+3. on prem services can connect to artifactory and the repo isn't cached - accessing most up to date version -3
+4. Publishes results to tools like Jira/Confluence page - 3
+5. Make extendable so that other package types -3
+"""
