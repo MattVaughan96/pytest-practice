b17e727edad4cef14cb494f32e963f6e1cdaef1a
Matt Vaughan <86467372+MattVaughan96@users.noreply.github.com>
2021-07-30 09:52:21 +0100

    Create POC_basic.py

---
+++ automated_testing/POC_basic.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+import artifactory
+
+username = 
+password = 
+
+path = artifactorypath(link, auth = (username, password))
+
+def test_response():
+  response = requests.get(path)
+  assert response blah blah == 200
+  
+
+#code that asserts a package can be downloaded.
